bc3a3f74e5d5fe7db0d1edea1c510f40db5f7eb7
AlexFlorenco <aflorenco13@gmail.com>
2024-02-28 22:01:14 -0300

    dist

--- livo-api/dist/infra/http/modules/user/viewModel/userViewModel.d.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "src/modules/user/entities/User";
-export declare class UserViewModel {
-    static toHttp({ id, nome, email }: User): {
-        id: string;
-        nome: string;
-        email: string;
-    };
-}
